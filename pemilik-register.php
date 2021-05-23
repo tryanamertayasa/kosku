c6e4c297167ae63eb814a3912dbe0a4055e863f3
tryanamertayasa <tryanamertayasa@gmail.com>
2021-05-24 07:09:45 +0800

    create login, register, and logout for admin area

---
+++ pemilik-register.php
@@ -0,0 +1,139 @@
+<?php 
+  require 'functions.php';
+
+  if(isset($_POST["register"])){
+    if(pemilikRegister($_POST) > 0){
+      echo "<script>
+            alert('Akun Berhasil Dibuat!');
+            window.location.replace('pemilik-login.php');
+          </script>";
+    } else {
+      //echo mysql_error($db);
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php
+        include("header.php");
+    ?>
+  </head>
+
+  <body>
+    <!-- Navigation -->
+    <?php
+        include('template\navbar.php');
+    ?>
+
+    <!-- Page Content -->
+    <div class="page-content page-auth mt-5" id="register">
+      <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4">
+              <h2>
+                Buat akunmu sekarang! <br />
+                Iklan Kos Jadi Mudah! 
+              </h2>
+              <form class="mt-3" method="post">
+                <div class="form-group">
+                  <label>Nama</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    aria-describedby="nameHelp"
+                    v-model="name"
+                    name="name"
+                    autofocus
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    aria-describedby="emailHelp"
+                    v-model="email"
+                    name="email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>No. HP</label>
+                  <input
+                    type="phone"
+                    class="form-control"
+                    aria-describedby="phoneHelp"
+                    v-model="phone"
+                    name="phone"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <input 
+                    type="password" 
+                    class="form-control" 
+                    name="password"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Alamat</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="address"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Kabupaten atau Kota</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="regencies"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Kode Pos</label>
+                  <input
+                    type="number"
+                    class="form-control"
+                    name="zip_code"
+                  />
+                </div>
+                <div class="form-group">
+                  <label>Tanggal Lahir</label>
+                  <input
+                    type="date"
+                    class="form-control"
+                    name="birth_date"
+                  />
+                </div>
+                <button type="submit" class="btn btn-success btn-block mt-4" name="register">
+                  Sign Up Now
+                </button>
+                <button type="submit" class="btn btn-signup btn-block mt-2">
+                  Back to Sign In
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+     <?php
+        include("footer.php")
+     ?>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.slim.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script src="script/navbar-scroll.js"></script>
+  </body>
+</html>
