667ce5550afb21a3925900eeb7021b0d010d7c42
tryanamertayasa <tryanamertayasa@gmail.com>
2021-05-24 07:08:19 +0800

    Create side bar for list kos

---
+++ template/_kos-pemilik-list.php
@@ -0,0 +1,42 @@
+<div class="dashboard-content">
+                <div class="tab-content" id="myTabContent">
+                <?php $i = 1; ?>
+                    <?php foreach ($kosdata as $row) : ?>
+                    <div class="row mt-3">
+                      <div class="col-12 mt-2">
+                        <a
+                          class="card card-list d-block"
+                          href="pemilik-iklan-details.php"
+                        >
+                          <div class="card-body">
+                            <div class="row">
+                              <div class="col-md-1">
+                                <img
+                                  src="images/dashboard-icon-product-1.png"
+                                  alt=""
+                                />
+                              </div>
+                              <div class="col-md-4">
+                                <?php echo $row['title'];?>
+                              </div>
+                              <div class="col-md-3">
+                              <?php echo $row['price'];?>
+                              </div>
+                              <div class="col-md-3">
+                              <?php echo $row['id_location'];?>
+                              </div>
+                              <div class="col-md-1 d-none d-md-block">
+                                <img
+                                  src="images/dashboard-arrow-right.svg"
+                                  alt=""
+                                />
+                              </div>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <?php $i++; ?>
+                  <?php endforeach; ?>
+                </div>
